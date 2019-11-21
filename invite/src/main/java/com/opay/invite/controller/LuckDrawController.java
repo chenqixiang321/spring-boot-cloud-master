b86c30ac41ecc73a8de9f008d3c7418f29f74bcd
zhangyixin <zhangyixin@mobimagic.com>
2019-11-21 10:31:08 +0800

    抽奖功能

--- invite/src/main/java/com/opay/invite/controller/LuckDrawController.java
+++ invite/src/main/java/com/opay/invite/controller/LuckDrawController.java
@@ -1,6 +1,9 @@
 package com.opay.invite.controller;
 
+import com.opay.invite.model.LuckDrawModel;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/luckDraw")
 @Api(value = "抽奖功能API")
 public class LuckDrawController {
+
+    @ApiOperation(value = "获取抽奖信息", notes = "获取抽奖信息")
+    @GetMapping("/info")
+    public LuckDrawModel getGroupInfo() {
+        return new LuckDrawModel();
+    }
 }
