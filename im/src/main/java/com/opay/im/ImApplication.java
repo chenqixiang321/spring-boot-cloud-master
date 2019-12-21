aab9be5cd22e78d69ec8ffcf622d3f1afa319f09
zhangyixin <zhangyixin@mobimagic.com>
2019-12-21 13:55:08 +0800

    修改调用接口超时时间
    Summary: 修改调用接口超时时间
    
    Test Plan: local test
    
    Reviewers: chenqixiang, lin.huang
    
    Reviewed By: chenqixiang
    
    Differential Revision: https://pha.static.kunlun.com/D18114

--- im/src/main/java/com/opay/im/ImApplication.java
+++ im/src/main/java/com/opay/im/ImApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients(clients = {OpayFeignApiService.class})
 @MapperScan(basePackages = "com.opay.im.mapper")
 @EnableScheduling
+@EnableCircuitBreaker
 //@EnableApolloConfig
 public class ImApplication {
 
--- invite/src/main/java/com/opay/invite/InviteApplication.java
+++ invite/src/main/java/com/opay/invite/InviteApplication.java
@@ -4,6 +4,7 @@ import com.opay.invite.service.OpayFeignApiService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableHystrix
 @EnableFeignClients(clients = {OpayFeignApiService.class})
 @EnableAsync
+@EnableCircuitBreaker
 public class InviteApplication {
 
     public static void main(String[] args) {
